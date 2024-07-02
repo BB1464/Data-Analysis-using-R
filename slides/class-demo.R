618bdc30b475a2ceab4a5a4dbe6328b51e7e7b6f
BB1464 <oluwafemioyedele908@gmail.com>
2024-07-02 10:52:04 -0500

    I added a demonstration script

---
+++ slides/class-demo.R
@@ -0,0 +1,10 @@
+
+# How to carry out addition in R ------------------------------------------
+
+addition <- 1+1 # addition
+
+addition
+
+2*5 # Multiplication
+
+print("I love coding in R") # print statement
